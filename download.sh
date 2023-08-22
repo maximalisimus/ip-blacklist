a753db32b0aed58df74752cb7685aec9b421efa9
maximalis171091@yandex.ru <maximalis171091@yandex.ru>
2023-08-22 11:21:04 +0300

    Update

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-curl https://files-wg.duckdns.org/ip-blacklist{-log.txt,.json} -o ./ip-blacklist-log.txt -o ./ip-blacklist.json
+curl https://files-wg.duckdns.org/ip-blacklist-log.txt -o ./ip-blacklist-log.txt
+curl https://files-wg.duckdns.org/ip-blacklist.json -o ./ip-blacklist.json
 exit 0
