14e7f8e8ef6a4e5959e36c395fd33e2b6b479ae0
Mikl <maximalis171091@yandex.ru>
2023-09-20 07:59:18 +0300

    Update

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#curl https://files-wg.duckdns.org/ip-blacklist-log.txt -o ./ip-blacklist-log.txt
-#curl https://files-wg.duckdns.org/ip-blacklist.json -o ./ip-blacklist.json
-wget https://files-wg.duckdns.org/ip-blacklist-log.txt
-wget https://files-wg.duckdns.org/ip-blacklist.json
+curl https://files-wg.duckdns.org/ip-blacklist-log.txt -o ./ip-blacklist-log.txt
+curl https://files-wg.duckdns.org/ip-blacklist.json -o ./ip-blacklist.json
+#wget https://files-wg.duckdns.org/ip-blacklist-log.txt
+#wget https://files-wg.duckdns.org/ip-blacklist.json
 exit 0
